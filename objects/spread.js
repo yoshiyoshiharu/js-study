835d0366ff6c61e1d5bde0758acc489c90485314
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-08 09:26:11 +0900

    study object spread

--- objects/spread.js
+++ objects/spread.js
@@ -8,3 +8,22 @@ function directions(...args) {
   const [start, ...remaining] = args;
   const [finish, ...stops] = remaining.reverse();
 }
+
+// オブジェクトのスプレッド構文
+const morning = {
+  breakfast: "oatmeal",
+  lunch: "peanut butter and jelly"
+};
+const dinner = "mac and cheese";
+
+const backpackingMeals = {
+  ...morning,
+  dinner
+};
+/*
+{
+  breakfast: "oatmeal",
+  lunch: "peanut butter and jelly",
+  dinner: "mac and cheese"
+}
+*/
